bf5a421a5bb218d8120aad4a9f63c76c44e7f179
mtellami <moadtell@gmail.com>
2024-03-28 03:20:40 +0000

    init

---
+++ src/main.py
@@ -0,0 +1,39 @@
+import paramiko
+import dotenv
+import os
+import time
+
+dotenv.load_dotenv()
+
+username = os.getenv('SSH_USER')
+password = os.getenv('SSH_PASSWORD')
+
+try:
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    client.connect(hostname='localhost', username=username, password=password)
+    channel = client.invoke_shell()
+    time.sleep(0.5)
+
+    # stdin, stdout, stderr = client.exec_command(command)
+
+    commands = [
+        'ls',
+        'whoami',
+        'pwd'
+    ]
+
+    for command in commands:
+        channel.send(command + '\n')
+        time.sleep(0.05)
+
+    output = ''
+    while channel.recv_ready():
+        output += channel.recv(1024).decode('utf-8')
+
+    print(output)
+
+    client.close()
+except Exception as err:
+    print(err)
