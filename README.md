40e87c4b04773854f0c1ac6ce556c516f3081e18
mtellami <moadtell@gmail.com>
2024-04-02 06:05:57 +0000

    containerization

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ### Remote Control
+
+<!-- open pipe -->
+browser <==> socket-server <==> ssh-client
+
+#### run
+```zsh
+    docker-compose up --build
+```
--- ui/src/components/Terminal.jsx
+++ ui/src/components/Terminal.jsx
@@ -1,48 +1,55 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Terminal as XTerm } from '@xterm/xterm';
 import '@xterm/xterm/css/xterm.css';
 
 const Terminal = () => {
   const terminalRef = useRef();
+	const [input, setInput] = useState('')
 	let socket;
 
   useEffect(() => {
     const terminal = new XTerm();
-    terminal.open(terminalRef.current);
 		try {
+			terminal.open(terminalRef.current);
+			terminal.write('\r\n ~ ')
 			socket = new WebSocket('ws://localhost:8000/ws/stream')
-			socket.onopen = (event) => {
+			socket.onopen = () => {
 				console.log('websocket connected')
 				socket.send(JSON.stringify({ event: "shellstream", command: "ls -l" }))
 				socket.onmessage = (event) => {
-					console.log(event)
-					terminal.write(event.data)
+					terminal.write('\r\n' + event.data)
+					setInput('')
 				}
 			}
 			socket.onerror = (error) => {
-				console.log('Websocket error: ', error)
+				// console.log('Websocket error: ', error)
 			}
+
+			terminal.onKey((event) => {
+				if (event.key.charCodeAt(0) >= 32 && event.key.charCodeAt(0) <= 126) {
+					setInput(input + event.key)
+					terminal.write(event.key)
+				} else if (event.key == '\r') {
+					socket.send(JSON.stringify({ event: "shellstream", command: input }))
+					terminal.write('\r\n ~ ')
+				} else if (event.key.charCodeAt(0) == 127 && input !== '') {
+					terminal.write('\b \b')
+					setInput(input.slice(0, input.length - 1))
+				}
+			})
+
+			terminal.focus()
 		} catch (error) {
 			console.log('Error: ', error)
 		}
 
-    terminal.write('Welcome to xterm.js\n\r');
-    
     return () => {
 			socket.close()
       terminal.dispose();
-    };
-  }, []);
+    }
+  }, [])
 
-  return (
-		<>
-			<div ref={terminalRef} />
-			<input type='text' id='test' />
-			<button onClick={(event) => socket.send(JSON.stringify({ event: "shellstream", command: document.getElementById('test')?.value }))}>
-				click
-			</button>
-		</>
-	)
+  return <div ref={terminalRef} />
 };
 
 export default Terminal;
