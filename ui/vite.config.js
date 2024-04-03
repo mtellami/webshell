19754af9dbe80244acb7ea50abfaa8d3a9963029
mtellami <moadtell@gmail.com>
2024-04-03 06:42:58 +0000

    start dev

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-	server: {
-		port: 4000
-	}
+	 server: {
+		port: 4000,
+		strictPort: true,
+		host: true,
+		origin: "http://0.0.0.0:4000",
+	 },
 })
